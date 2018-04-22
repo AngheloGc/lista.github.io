caa0c698192b238b6bbbe6273fb191e4926631f1
Anghelo <agiron_1907@hotmail.com>
2018-04-22 00:48:26 -0500

    Initial commit

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+window.addEventListener('load', function() {
+
+	for (var i = 0; i < lista.items.length; i++) {
+
+		var item = document.createElement("div");
+
+		item.className = 'item';
+
+    	item.innerHTML = "<img src="+lista.items[i].url+"><p class='SongName'>"+lista.items[i].song+"</p><span onclick='DeleteItem(this)' class='deleteItem'>X</span>";
+
+   		 document.getElementById('main').appendChild(item);
+	}
+  
+})
+
+
 function AddItem() {
 
 	var url = document.getElementById("url").value;
@@ -21,14 +37,5 @@ function DeleteItem(e) {
 
 $.getJSON('lista.json', function(data) {
 
-	for (var i = 0; i < data.items.length; i++) {
-
-		var item = document.createElement("div");
-
-		item.className = 'item';
-
-    	item.innerHTML = "<img src="+data.items[i].url+"><p class='SongName'>"+data.items[i].song+"</p><span onclick='DeleteItem(this)' class='deleteItem'>X</span>";
-
-   		 document.getElementById('main').appendChild(item);
-	}
+	
 });
